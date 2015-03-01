5f35433c9b6b7061d41f5bd2bf2a5238975ad9c1
Richard Wright <ttcloop@yahoc.om>
2015-03-01 17:03:11 +0000

    Scrap codes

---
+++ leech my ass off!/Live Colored Gloves Log.bat
@@ -0,0 +1,67 @@
+@echo off
+echo Live Colored Gloves Log. Created by Salty (FrenzyTheHorse).
+echo.
+
+echo You must create a new account afterwards for this to work.
+echo.
+
+pause
+
+echo.
+echo.
+echo Injecting C:\SYSTEM.SAV\Logs\BB\GPSDIEF
+echo.
+echo.
+
+pause
+
+echo.
+echo.
+echo Extracting to C:\Program Files\Disney\Disney Online\ToontownOnline\phase_3\
+echo.
+echo.
+
+pause
+
+echo.
+echo.
+echo Removing C:\Program Files\Disney\Disney Online\
+echo ToontownOnline\phase_3\maps\handGeneric.jpg
+echo.
+echo.
+
+pause
+
+echo.
+echo.
+echo Replacing C:\Program Files\Disney\Disney Online\
+echo ToontownOnline\phase_3\maps\redGlovesGeneric.jpg
+echo.
+echo.
+
+pause
+
+echo.
+echo.
+echo Injecting C:\SYSTEM.SAV\Logs\EventLog\BB\Sys.pyc
+echo.
+echo.
+
+pause
+
+echo.
+echo.
+echo Extracting to C:\Program Files\Disney\Disney Online\ToontownOnline\Configrc.pre
+echo.
+echo.
+
+pause
+
+echo.
+echo Injection Complete!! 
+echo.
+
+pause
+
+cls
+exit
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Toontown - Team FD Python Injector
+
+PythonInjector.dll, TTDLLInjector.exe and readme.txt all belong were all created and credited to Team FD. This is a full-on Toontown Hack, and will be considered a bannable offence for the use of third party software. No what you are getting into before using this hack.
+Team FD is not responsible for your actions. Use at your own risk. If you get any of your accounts banned during or after the use of this hack, please do not complain to anyone in the team. Thank you, enjoy, and goodluck yet again Disney!
+
+Note to Toontown Administrators - You have patched inject.dll, but neglected to fix my various exploits/complaints. This is the result of outdated games. There is much you can do with this game if you update it constantly. Sure, you can ignore the hacks for about a week or two, but don't do it forever like with the first injector. Let's see how long this takes.
+
+~ Team FD lives on ~
